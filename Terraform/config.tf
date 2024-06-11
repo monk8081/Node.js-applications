dda197bfb79afe548df5fd72dab24777d4ab4df5
Manish Kumar <113243067+monk8081@users.noreply.github.com>
2024-06-11 10:35:34 +0530

    Update config.tf

--- Terraform/config.tf
+++ Terraform/config.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAUFJZBDC5RHTFMANK"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
